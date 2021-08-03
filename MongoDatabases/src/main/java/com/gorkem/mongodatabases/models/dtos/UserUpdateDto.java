c27f539c0449f2d1bf3ef8bb99d59180959443bc
Gorkemblm <gorkemblm@gmail.com>
2021-08-03 16:45:18 +0300

    added update crud operation dto

---
+++ MongoDatabases/src/main/java/com/gorkem/mongodatabases/models/dtos/UserUpdateDto.java
@@ -0,0 +1,22 @@
+package com.gorkem.mongodatabases.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateDto {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String identityNumber;
+    private LocalDate dateOfBirth;
+    private String address;
+    private String phoneNumber;
+}
